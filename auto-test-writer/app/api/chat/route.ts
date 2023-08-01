d8c5ae68542f5b96a4e351481ecda6919812a972
Samir <118690585+sr5434@users.noreply.github.com>
2023-08-01 18:21:22 +0000

    Upload code

---
+++ auto-test-writer/app/api/chat/route.ts
@@ -0,0 +1,34 @@
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(config);
+
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  const { code } = await req.json();
+  console.log(code);
+  // Ask OpenAI for a streaming completion given the prompt
+  //console.log(bio)
+  //console.log(vibe)
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+      {
+        role: 'user',
+        content: `When given a program, if it is written in JavaScript, only write tests in Jest.
+        If it is written in python, only write tests using unittest. With that said, write tests for the following code: ${code}`,
+      },
+    ],
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
